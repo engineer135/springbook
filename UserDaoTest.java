500a05f82396f96eae8d267c5f959c20324481d8
engineer135 <everafterk@naver.com>
2016-06-06 22:22:50 +0900

    본격적인 스프링 적용.

--- UserDaoTest.java
+++ UserDaoTest.java
@@ -1,5 +1,8 @@
 import java.sql.SQLException;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import springbook.user.dao.ConnectionMaker;
 import springbook.user.dao.DConnectionMaker;
 import springbook.user.dao.DaoFactory;
@@ -20,8 +23,16 @@ public class UserDaoTest {
 		UserDao dao = new UserDao(connectionMaker);
 		*/
 		
-		DaoFactory daoFactory = new DaoFactory();
-		UserDao dao = daoFactory.userDao();
+		/*DaoFactory daoFactory = new DaoFactory();
+		UserDao dao = daoFactory.userDao();*/
+		
+		// 애플리케이션 컨텍스트를 적용
+		// 이렇게 하면 클라이언트는 구체적인 팩토리 클래스를 알 필요가 없다. 일관된 방식으로 원하는 오브젝트를 가져올 수 있다.
+		// 의존관계 검색(Dependency Lookup) - 의존관계 주입(Dependency Injection)이 불가능한 경우 사용
+		// 검색하는 오브젝트는 자신이 스프링의 빈일 필요가 없음.
+		// DI는 주입되는 오브젝트도 반드시 빈 오브젝트여야 함.
+		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		UserDao dao = context.getBean("userDao", UserDao.class); // 첫번째 인자는 빈의 이름, 두번째 인자는 리턴 타입
 		
 		User user = new User();
 		user.setId("test1");
--- springbook/user/dao/DaoFactory.java
+++ springbook/user/dao/DaoFactory.java
@@ -1,5 +1,8 @@
 package springbook.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @author Engineer135
  *
@@ -8,7 +11,13 @@ package springbook.user.dao;
  * UserDao, ConnectionMaker 는 각각 데이터 로직, 기술 로직 담당. 
  * DaoFactory는 어떤 오브젝트가 어떤 오브젝트를 사용하는지를 정의해놓은 설계도라고 보면 된다.
  */
+
+// 스프링이 제어권을 가지고 직접 만들고 관계를 부여하는 오브젝트를 빈(Bean)이라고 부른다.
+// 빈의 생성과 관계설정 같은 제어를 담당하는 IoC 오브젝트를 빈팩토리 라고 부른다. 이를 확장한 것이 애플리케이션 컨텍스트.
+@Configuration //애플리케이션 컨텍스트 또는 빈 팩토리가 사용할 설정정보라는 표시
 public class DaoFactory {
+	
+	@Bean // 오브젝트 생성을 담당하는 IoC용 메소드라는 표시
 	public UserDao userDao(){
 		return new UserDao(connectionMaker());
 	}
@@ -21,8 +30,11 @@ public class DaoFactory {
 		return new MessageDao(connectionMaker());
 	}*/
 	
+	@Bean // 오브젝트 생성을 담당하는 IoC용 메소드라는 표시
 	// 분리해서 중복을 제거한 ConnectionMaker 타입 오브젝트 생성 코드
 	public ConnectionMaker connectionMaker(){
 		return new DConnectionMaker();
+		// 이런식으로 로컬 DB, 운영 DB 나눠서 사용도 가능. 이 부분만 수정하면  DB 접속 정보가 바뀌므로. 얼마나 편한가!
+		//return new NConnectionMaker();
 	}
 }
