57aa994d39e1df1d3bd58fd19889936f5cb7045b
engineer135 <everafterk@naver.com>
2016-06-06 23:30:21 +0900

    Bean 설정을 클래스가 아닌 XML로 변경한다.

--- UserDaoTest.java
+++ UserDaoTest.java
@@ -2,6 +2,7 @@ import java.sql.SQLException;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 import springbook.user.dao.ConnectionMaker;
 import springbook.user.dao.DConnectionMaker;
@@ -31,7 +32,11 @@ public class UserDaoTest {
 		// 의존관계 검색(Dependency Lookup) - 의존관계 주입(Dependency Injection)이 불가능한 경우 사용
 		// 검색하는 오브젝트는 자신이 스프링의 빈일 필요가 없음.
 		// DI는 주입되는 오브젝트도 반드시 빈 오브젝트여야 함.
-		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		
+		//ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		
+		// XML을 이용하는 애플리케이션 컨텍스트 적용
+		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
 		UserDao dao = context.getBean("userDao", UserDao.class); // 첫번째 인자는 빈의 이름, 두번째 인자는 리턴 타입
 		
 		User user = new User();
