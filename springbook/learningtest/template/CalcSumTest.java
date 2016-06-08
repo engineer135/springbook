53ff7b96e6835cc01fd3a3a9a07d6ec2294d9d8f
engineer135 <everafterk@naver.com>
2016-06-08 19:07:06 +0900

    템플릿/콜백의 재설계 코드의 특성이 바뀌는 경계를 잘 살피고 그것을 인터페이스를 사용해 분리하는것... 어렵네 ㅠㅠ

--- springbook/learningtest/template/CalcSumTest.java
+++ springbook/learningtest/template/CalcSumTest.java
@@ -5,19 +5,32 @@ import static org.junit.Assert.assertThat;
 
 import java.io.IOException;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalcSumTest {
 	
-	@Test
-	public void sumOfNumbers() throws IOException {
-		Calculator calculator = new Calculator();
+	Calculator calculator;
+	String numFilePath;
+	
+	@Before
+	public void setUp(){
+		this.calculator =  new Calculator();
 		/*절대경로
 		this.getClass().getResource("").getPath(); // 현재 자신의 절대 경로
 		this.getClass().getResource("/").getPath(); // classes 폴더의 최상위 경로
 		this.getClass().getResource("/com/test/config/config.properties").getPath(); // classes 폴더에서부터 시작하여 해당파일까지의 절대 경로
 		 */		
-		int sum = calculator.calcSum(getClass().getResource("numbers.txt").getPath());
-		assertThat(sum, is(10));
+		this.numFilePath = getClass().getResource("numbers.txt").getPath();
+	}
+	
+	@Test
+	public void sumOfNumbers() throws IOException {
+		assertThat(calculator.calcSum(this.numFilePath), is(10));
+	}
+	
+	@Test
+	public void multiplyOfNumbers() throws IOException {
+		assertThat(calculator.calcMultiply(this.numFilePath), is(24));
 	}
 }
--- springbook/learningtest/template/Calculator.java
+++ springbook/learningtest/template/Calculator.java
@@ -6,16 +6,90 @@ import java.io.IOException;
 
 public class Calculator {
 	public Integer calcSum(String filePath) throws IOException {
+		/*BufferedReaderCallback sumCallback = new BufferedReaderCallback(){
+			public Integer doSomethingWithReader(BufferedReader br) throws IOException {
+				Integer sum = 0;
+				String line = null;
+				while((line = br.readLine()) != null){//마지막 라인까지 한줄씩 읽어가면서 숫자를 더한다.
+					sum += Integer.valueOf(line);
+				}
+				return sum;
+			}
+		};
+		
+		return fileReadTemplate(filePath, sumCallback);*/
+		
+		LineCallback sumCallback = new LineCallback(){
+			public Integer doSomethingWithLine(String line, Integer value){
+				return value + Integer.valueOf(line);
+			}
+		};
+		
+		return lineReadTemplate(filePath, sumCallback, 0);
+	}
+	
+	public Integer calcMultiply(String filePath) throws IOException {
+		/*BufferedReaderCallback multiplyCallback = new BufferedReaderCallback(){
+			public Integer doSomethingWithReader(BufferedReader br) throws IOException {
+				Integer multiply = 1;
+				String line = null;
+				while((line = br.readLine()) != null){//마지막 라인까지 한줄씩 읽어가면서 숫자를 더한다.
+					multiply *= Integer.valueOf(line);
+				}
+				return multiply;
+			}
+		};
+		
+		return fileReadTemplate(filePath, multiplyCallback);*/
+		
+		LineCallback multiplyCallback = new LineCallback(){
+			public Integer doSomethingWithLine(String line, Integer value){
+				return value * Integer.valueOf(line);
+			}
+		};
+		
+		return lineReadTemplate(filePath, multiplyCallback, 1);
+	}
+	
+	public Integer fileReadTemplate(String filePath, BufferedReaderCallback callback) throws IOException {
 		BufferedReader br = null;
 		
 		try{
 			br = new BufferedReader(new FileReader(filePath)); //한줄씩 읽기 편하게 BufferedReader로 파일을 가져온다
-			Integer sum = 0;
-			String line = null;
-			while((line = br.readLine()) != null){//마지막 라인까지 한줄씩 읽어가면서 숫자를 더한다.
-				sum += Integer.valueOf(line);
+			
+			// 콜백 오브젝트 호출. 템플릿에서 만든 컨텍스트 정보인 BufferedReader를 전달해주고 콜백의 작업 결과를 받아둔다.
+			int ret = callback.doSomethingWithReader(br);
+			
+			return ret;
+		}catch(IOException e){
+			System.out.println(e.getMessage());
+			throw e;
+		}finally{
+			if(br != null){//반드시 null체크 해줘야 함!
+				try{
+					br.close();
+				}catch(IOException e){
+					System.out.println(e.getMessage());
+					throw e;
+				}
 			}
-			return sum;
+		}
+	}
+	
+	public Integer lineReadTemplate(String filePath, LineCallback callback, int initVal) throws IOException {
+		BufferedReader br = null;
+		
+		try{
+			br = new BufferedReader(new FileReader(filePath)); //한줄씩 읽기 편하게 BufferedReader로 파일을 가져온다
+			
+			Integer res = initVal;
+			String line = null;
+			
+			while((line = br.readLine()) != null){//파일의 각 라인을 루프를 돌면서 가져오는 것도 템플릿이 담당한다.
+				res = callback.doSomethingWithLine(line, res);//각 라인의 내용을 가지고 계산하는 작업만 콜백에게 맡긴다.
+			}
+			
+			return res;
 		}catch(IOException e){
 			System.out.println(e.getMessage());
 			throw e;
