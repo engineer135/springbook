223e379489f03aa90b590b5e0a6a6383291ed74c
engineer135 <everafterk@naver.com>
2016-06-10 10:28:02 +0900

    upgradeLevels() 리팩토링

--- springbook/user/service/UserService.java
+++ springbook/user/service/UserService.java
@@ -22,28 +22,34 @@ public class UserService {
 	public void upgradeLevels(){
 		List<User> users = userDao.getAll();
 		for(User user : users){
-			Boolean changed = null; //레벨 변화가 있는지 확인하는 플래그
-			if(user.getLevel() == Level.BASIC && user.getLogin() >= 50){//Basic 레벨 업그레이드 작업
-				user.setLevel(Level.SILVER);
-				changed = true;
-			}else if(user.getLevel() == Level.SILVER && user.getRecommend() >= 30){//Silver 레벨 업그레이드 작업
-				user.setLevel(Level.GOLD);
-				changed = true;
-			}else if(user.getLevel() == Level.GOLD){//Gold 레벨은 변경 없음
-				changed = false;
-			}else{
-				changed = false;
-			}
-			
-			if(changed){//레벨 변경이 있는 경우에만 update() 호출
-				userDao.update(user);
+			if(canUpgradeLevel(user)){
+				upgradeLevel(user);
 			}
 		}
+	}
+	
+	private boolean canUpgradeLevel(User user) {
+		Level currentLevel = user.getLevel();
+		switch(currentLevel){
+			case BASIC : return (user.getLogin() >= 50);
+			case SILVER : return (user.getRecommend() >= 30);
+			case GOLD : return false;
+			default : throw new IllegalArgumentException("Unknown Leve: "+ currentLevel);
+		}
+	}
+	
+	private void upgradeLevel(User user){
+		if(user.getLevel() == Level.BASIC){
+			user.setLevel(Level.SILVER);
+		}else if(user.getLevel() == Level.SILVER){
+			user.setLevel(Level.GOLD);
+		}
+		userDao.update(user);
 	}
 
 	public void add(User user) {
 		if(user.getLevel() == null){
-			user.setLevel(Level.BASIC);
+			user.setLevel(Level.BASIC); 
 		}
 		userDao.add(user);
 	}
