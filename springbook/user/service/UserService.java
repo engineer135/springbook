54ea7665698865295ca6e5bf009c42d9641a7b69
engineer135 <everafterk@naver.com>
2016-06-13 11:13:33 +0900

    PlatformTransactionManager 스프링이 제공하는 추상화 기능 사용

--- springbook/user/service/UserService.java
+++ springbook/user/service/UserService.java
@@ -7,7 +7,12 @@ import java.util.List;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.jta.JtaTransactionManager;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import springbook.user.dao.UserDao;
@@ -44,9 +49,21 @@ public class UserService {
 
 	//사용자 레벨 업그레이드 메소드
 	public void upgradeLevels() throws Exception{
-		TransactionSynchronizationManager.initSynchronization();//트랜잭션 동기화 관리자를 이용해 동기화 작업을 초기화한다.
-		Connection c = DataSourceUtils.getConnection(dataSource); //DB 커넥션을 생성하고 트랜잭션을 시작한다. 이후의 DAO 작업은 모두 여기서 시작한 트랜잭션 안에서 진행된다.
-		c.setAutoCommit(false);
+		//TransactionSynchronizationManager.initSynchronization();//트랜잭션 동기화 관리자를 이용해 동기화 작업을 초기화한다.
+		//Connection c = DataSourceUtils.getConnection(dataSource); //DB 커넥션을 생성하고 트랜잭션을 시작한다. 이후의 DAO 작업은 모두 여기서 시작한 트랜잭션 안에서 진행된다.
+		//c.setAutoCommit(false);
+		
+		// 스프링이 제공하는 트랜잭션 추상화 기술 적용(JDBC, JTA, 하이버네이트 등등에 이거 하나로 적용이 가능!)
+		PlatformTransactionManager transactionManager = new DataSourceTransactionManager(dataSource);//JDBC 트랜잭션 추상 오브젝트 생성
+		
+		//PlatformTransactionManager transactionManager = new JtaTransactionManager(); JTA는 이런식으로 사용.. 하이버네이트도 마찬가지
+		// 하지만 어떤 트랜잭션 매니저 구현 클래스를 사용할지 UserService 코드가 알고 있는 것은 DI 원칙에 위배된다!
+		// 그러므로, 스프링 빈으로 등록 후 UserService가 DI 방식으로 사용하게 해야 한다.
+		// 여기서 중요한 것은, 어떤 클래스든 스프링 빈으로 등록할때 
+		// 싱글톤으로 만들어져 여러 스레드에서 동시에 사용해도 괜찮은가!? 하는 것이다.
+		// 상태를 갖고 있고, 멀티스레드 환경에서 안전하지 않은 클래스를 빈으로 등록하면 심각한 문제가 발생!
+		
+		TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
 		
 		try{
 			List<User> users = userDao.getAll();
@@ -56,17 +73,23 @@ public class UserService {
 				}
 			}
 			
-			c.commit(); //정상적으로 작업을 마치면 트랜잭션 커밋
+			//c.commit(); //정상적으로 작업을 마치면 트랜잭션 커밋
+			
+			transactionManager.commit(status);
 		}catch(Exception e){
-			c.rollback(); //예외가 발생하면 롤백
+			//c.rollback(); //예외가 발생하면 롤백
+			
+			transactionManager.rollback(status);
 			throw e;
-		}finally{
+		}
+		
+		/*finally{
 			DataSourceUtils.releaseConnection(c, dataSource);//스프링 유틸리티 메소드를 이용해 DB 커넥션을 안전하게 닫는다.
 			
 			// 동기화 작업 종료 및 정리
 			TransactionSynchronizationManager.unbindResource(this.dataSource);
 			TransactionSynchronizationManager.clearSynchronization();
-		}
+		}*/
 		
 		
 	}
