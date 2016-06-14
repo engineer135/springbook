aa134c44d56ee62352ae0b675955d2a7dedb5085
engineer135 <everafterk@naver.com>
2016-06-14 16:11:23 +0900

    다이내믹 프록시 적용 InvocationHadler를 이용한 다이내믹 프록시 구현

--- springbook/learningtest/jdk/ReflectionTest.java
+++ springbook/learningtest/jdk/ReflectionTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
 import org.junit.Test;
 
@@ -34,7 +35,14 @@ public class ReflectionTest {
 		assertThat(hello.sayThankYou("Toby"), is("Thank You Toby"));
 		
 		//프록시를 통해 타깃 오브젝트에 접근하도록 구성한다.
-		Hello proxiedHello = new HelloUppercase(new HelloTarget());
+		//Hello proxiedHello = new HelloUppercase(new HelloTarget());
+		
+		//InvocationHandler를 구현한 다이내믹 프록시 생성
+		Hello proxiedHello = (Hello) Proxy.newProxyInstance(
+										getClass().getClassLoader(), //동적으로 생성되는 다이내믹 프록시 클래스의 로딩에 사용할 클래스 로더
+										new Class[]{Hello.class}, //구현할 인터페이스
+										new UppercaseHandler(new HelloTarget())); //부가기능과 위임 코드를 담은 invocationHandler
+		
 		assertThat(proxiedHello.sayHello("Toby"), is("HELLO TOBY"));
 		assertThat(proxiedHello.sayHi("Toby"), is("HI TOBY"));
 		assertThat(proxiedHello.sayThankYou("Toby"), is("THANK YOU TOBY"));
