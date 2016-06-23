00d4a6208099529c5dd661e391bf82eceda7a64c
engineer135 <everafterk@naver.com>
2016-06-23 23:03:22 +0900

    <Bean>의 전환 XML의 dataSource를 자바 코드로 전환 후 XML의 dataSource를 주석 처리하고 테스트를 돌리면 성공한다. 이처럼 @Configuration 자바 클래스에서 정의한 빈과 XML에서 정의한 빈은 서로 참조가 가능하다. 쩐다!

--- springbook/TestApplicationContext.java
+++ springbook/TestApplicationContext.java
@@ -1,7 +1,11 @@
 package springbook;
 
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 /**
  * @author Engineer135
@@ -13,5 +17,18 @@ import org.springframework.context.annotation.ImportResource;
 @Configuration
 @ImportResource("/applicationContext.xml") //XML의 DI 정보를 활용한다.
 public class TestApplicationContext {
-
+	
+	@Bean
+	public DataSource dataSource(){
+		// 왜 DataSource가 아닌 SimpleDriverDataSource로 선언하는가?
+		// DataSource에는 setUrl이나 setUsername 같은 수정자 메소드가 없거든. 우린 이게 필요한데!
+		SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+		
+		dataSource.setDriverClass(org.mariadb.jdbc.Driver.class);
+		dataSource.setUrl("jdbc:mariadb://localhost/springbook?characterEncoding=utf-8");
+		dataSource.setUsername("spring");
+		dataSource.setPassword("book");
+		
+		return dataSource;
+	}
 }
